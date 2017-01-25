579c1694298efc6ec3bbc90cce9e501b89bbcea4
swilso421 <swilso421@gmail.com>
2017-01-25 13:52:10 -0500

    Updated 404.js
    Fixed file paths

--- scripts/404.js
+++ scripts/404.js
@@ -1,10 +1,10 @@
 images = new Array(5);
 
-images[0] = 'resources/404-1.jpg';
-images[1] = 'resources/404-2.jpg';
-images[2] = 'resources/404-3.jpg';
-images[3] = 'resources/404-4.png';
-images[4] = 'resources/404-5.png';
+images[0] = '/resources/images/404-1.jpg';
+images[1] = '/resources/images/404-2.jpg';
+images[2] = '/resources/images/404-3.jpg';
+images[3] = '/resources/images/404-4.png';
+images[4] = '/resources/images/404-5.png';
 
 function getImage() {
     n = Math.floor(Math.random() * images.length);
